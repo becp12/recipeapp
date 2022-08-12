c3df808de1358f82f000fe682979880dfa21b495
anthonybrockett <ajbrockett1991@gmail.com>
2022-08-12 15:59:55 -0700

    Add log out and sign up functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('accounts/signup/', views.signup, name='signup'),
+    path('recipes/', views.recipes_index, name='index'),
+
+
 ]
+
+# Need to update for the login redirect url {settings.py} to recipe index
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+# from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+# Will need to passed into CBV
+# from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def home(request):
@@ -7,3 +12,21 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+@login_required
+def recipes_index(request):
+  return render(request, 'recipes/index.html')    
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
