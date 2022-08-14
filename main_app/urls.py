806af66688c8c69eb3c32d97325010e78fbcbf50
loex345 <maitlandeg@gmail.com>
2022-08-14 13:14:00 -0700

    Add update and delete functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
     path('recipes/', views.recipes_index, name='index'),
-    path('recipes/<int:recipe_id>/', views.recipes_detail, name='detail'),
     path('recipes/create/', views.RecipeCreate.as_view(), name='recipes_create'),
+    path('recipes/<int:recipe_id>/', views.recipes_detail, name='detail'),
+    path('recipes/<int:pk>/update/', views.RecipeUpdate.as_view(), name='recipe_update'),
+    path('recipes/<int:pk>/delete/', views.RecipeDelete.as_view(), name='recipe_delete'),
 
 
 ]
