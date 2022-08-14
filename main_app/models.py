ca4228999a4f100921b3d8b0f0dfb4db26839a3c
loex345 <maitlandeg@gmail.com>
2022-08-14 10:12:20 -0700

    Add Recipe model test in shell

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,32 @@
+from sre_constants import CATEGORY
+from sre_parse import CATEGORIES
+from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+CATEGORIES = (
+    ('VT', 'Vegetarian'),
+    ('VE', 'Vegan'),
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    chef = models.ForeignKey(User, on_delete=models.CASCADE)
+    servings = models.IntegerField()
+    preptime = models.IntegerField()
+    cookingtime = models.IntegerField()
+    category = models.CharField(
+        max_length=2,
+        choices=CATEGORIES,
+        default=CATEGORIES[0][0]
+    )
+    method = models.TextField(max_length=1000)
+    # ingredients = models.ManyToManyField(Ingredient)
+
+    def __str__(self):
+        return self.title
+
