77a73de650646c4063c1feb2603092a977ebe312
becp12 <rebecca-preece@outlook.com>
2022-08-18 12:08:56 -0700

    Removing all unnecessary comments & dead code

--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,6 @@ import boto3
 from random import choice
 
 from django.shortcuts import render, redirect
-# from django.http import HttpResponse
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
